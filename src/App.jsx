7b8785514e11c7b9976b5873d98436ed55a26e3e
ematol <ematolr@gmail.com>
2024-09-15 13:51:44 -0600

    simplicity of react three fiber understood

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
+import { OrbitControls } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import { emissive } from "three/webgpu";
+
+const RotatingCube = () => {
+  const meshRef = useRef();
+
+  useFrame( () => {
+    if(meshRef.current) {
+      meshRef.current.rotation.y += 0.01
+      meshRef.current.rotation.x += 0.01
+    }
+  })
+  
+  return (
+    <mesh ref={meshRef}>
+      <cylinderGeometry args={[1, 1, 1]} />
+      <meshLambertMaterial color="#468585" emissive="468585" />
+    </mesh>
+  )
+}
+
+
 const App = () => {
   return (
-    <div>
-      Hello Three.js
-    </div>
+    <Canvas style={{ height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignContent: 'center' }}>
+      
+      <OrbitControls enableZoom enablePan enableRotate />
+      
+      <directionalLight position={[1, 1, 1]} intensity={10} color={0x9CDBA6} />
+
+      <color attach="background" args={['F0F0F0']} />
+
+      <RotatingCube />
+
+    </Canvas>
   )
 }
 
